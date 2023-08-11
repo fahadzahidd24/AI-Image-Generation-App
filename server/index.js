989afe25396870a037fa4cfb82b5d4b00551da15
unknown <fahadzahidd24@gmail.com>
2023-08-12 00:35:16 +0500

    Final

--- server/index.js
+++ server/index.js
@@ -10,6 +10,9 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
+app.use('/',(req,res)=>{
+    res.send('Welcome to G-Image')
+})
 app.use('/api/v1/posts', postRoutes);
 app.use('/api/v1/g-image', GImageRoutes);
 
